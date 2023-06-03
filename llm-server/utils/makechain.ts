54fcc71d817cff8535ff77662bd2427ff673f6f2
gharbat <arbioun@gmail.com>
2023-06-03 23:44:08 +0200

    Roll back the original inital prompt

--- llm-server/utils/makechain.ts
+++ llm-server/utils/makechain.ts
@@ -29,8 +29,9 @@ function getInitalPrmoptByMode(mode: string) {
 Chat History:
 {chat_history}
 Follow Up Input: {question}
-Standalone question:`, qa_prompt: `You are a helpful AI pair programmer. You are helping a human programmer with their code. You are answering questions about the given code.
-only answer questions that are about the code in the given context. If the question is not about the code in the context, answer with "I only answer questions about the code in the given context".
+Standalone question:`, qa_prompt: `You are a helpful AI customer support agent. Use the following pieces of context to answer the question at the end.
+If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
+If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
 {context}
 
