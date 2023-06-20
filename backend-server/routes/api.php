452f25760a532cd0500b1cbd6518bb0d83ea5034
gharbat <arbioun@gmail.com>
2023-06-20 02:17:55 +0200

    Fix: missing search endpoints

--- backend-server/routes/api.php
+++ backend-server/routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Api\Controllers\MessageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('chat/search', [MessageController::class, 'sendSearchRequest']);
