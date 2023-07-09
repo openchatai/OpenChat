cc6d0cd438a2c81fbc4e669648c0aa52189f4c13
gharbat <arbioun@gmail.com>
2023-07-09 19:43:59 +0200

    feat(bubble): add new API endpoint to handle incoming messages

--- backend-server/app/Http/Api/Controllers/MessageController.php
+++ backend-server/app/Http/Api/Controllers/MessageController.php
@@ -51,4 +51,110 @@ class MessageController extends Controller
         }
     }
 
+    public function initChat(Request $request): JsonResponse
+    {
+        $botToken = $request->header('X-Bot-Token');
+
+        /** @var Chatbot $bot */
+        $bot = Chatbot::where('token', $botToken)->first();
+
+        if (!$bot) {
+            return response()->json(
+                [
+                    "type" => "text",
+                    "response" => [
+                        "text" => "Could not find with token $botToken"
+                    ]
+                ]
+            );
+        }
+
+        if ($botToken == '1RuzS7w5ceGaN6CiK0J7'){
+            $initialQuestions = [
+                'What is OpenChat?',
+                'How I can start with OpenChat?',
+                'Can I embed my chatbot on my website?',
+                'Is OpenChat open source?'
+            ];
+        }
+
+        if ($botToken == '1RuzS7w5ceGaN6CiK0J7') {
+            $faq =
+                [
+                    [
+                        "id" => 1,
+                        "content" => "This is an example on article you can add in your chat bubble, it support **markdown** and more!",
+                        "title" => "Example on article"
+                    ]
+                ];
+        }
+
+        return response()->json(
+            [
+                "bot_name" => $bot->getName(),
+                "logo" => "logo",
+                "faq" => $faq?? [],
+                "inital_questions" => $initialQuestions ?? []
+            ]
+        );
+    }
+
+    public function sendChat(Request $request)
+    {
+        $this->validate($request, [
+            'content' => 'required|string|max:255',
+            'history' => 'sometimes|array',
+            'type' => 'in:text,button,email,emoji'
+        ]);
+
+        $botToken = $request->header('X-Bot-Token');
+        /** @var Chatbot $bot */
+        $bot = Chatbot::where('token', $botToken)->first();
+
+        if (!$bot) {
+            return response()->json(
+                [
+                    "type" => "text",
+                    "response" => [
+                        "text" => "I'm unable to help you at the moment, please try again later.  **code: b404**"
+                    ]
+                ]
+            );
+        }
+
+        $message = $request->input('content');
+
+
+        // otherwise, send the message to the bot
+        $response = Http::timeout(200)->post('http://llm-server:3000/api/chat', [
+            'question' => $message,
+            'namespace' => $bot->getId()->toString(),
+            'mode' => "assistant",
+            'initial_prompt' => $bot->getPromptMessage(),
+            'history' => $request->input('history')
+        ]);
+
+        if (is_null($response->json())) {
+            return response()->json(
+                [
+                    "type" => "text",
+                    "response" => [
+                        "text" => "The request was received successfully, but the LLM server was unable to handle it, please make sure
+                        your env keys are set correctly. **code: llm5XX**"
+                    ]
+                ]
+            );
+        }
+        $botResponse = new ChatbotResponse($response->json());
+
+
+        return response()->json(
+            [
+                "type" => "text",
+                "response" => [
+                    "text" => $botResponse->getBotReply()
+                ]
+            ]
+        );
+    }
 }
