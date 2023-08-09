84eea999f528cbb9586963bf9fbf7950c50acce4
Shanur Rahman <shanur.cse.nitap@gmail.com>
2023-08-09 15:24:42 +0300

    Fixed embedding support for azure and openai providers, now we can use them both at the same time...

--- dj_backend_server/api/enums/embedding_type.py
+++ dj_backend_server/api/enums/embedding_type.py
@@ -4,3 +4,5 @@ from enum import Enum
 class EmbeddingProvider(Enum):
     OPENAI = "openai"
     BARD = "bard"
+    azure = "azure"
+
--- dj_backend_server/api/utils/get_openai_llm.py
+++ dj_backend_server/api/utils/get_openai_llm.py
@@ -8,15 +8,21 @@ load_dotenv()
 def get_azure_openai_llm():
     """Returns AzureOpenAI instance configured from environment variables"""
     
-    openai_api_key = os.environ['OPENAI_API_KEY']
-    openai_deployment_name = os.environ['OPENAI_DEPLOYMENT_NAME']
-    openai_model_name = os.environ['OPENAI_COMPLETION_MODEL']
-
+    openai_api_type = os.environ['OPENAI_API_TYPE']
+    openai_api_key = os.environ['AZURE_OPENAI_API_KEY']
+    openai_deployment_name = os.environ['AZURE_OPENAI_DEPLOYMENT_NAME']
+    openai_model_name = os.environ['AZURE_OPENAI_COMPLETION_MODEL']
+    openai_api_version = os.environ['AZURE_OPENAI_API_VERSION']
+    openai_api_base=os.environ['AZURE_OPENAI_API_BASE']
     return AzureOpenAI(
+        openai_api_base=openai_api_base,
         openai_api_key=openai_api_key,
         deployment_name=openai_deployment_name,
         model_name=openai_model_name,
+        openai_api_type=openai_api_type,
+        openai_api_version=openai_api_version,
         temperature=0,
+        batch_size=8
     )
 
 # OpenAI Language Model client  
