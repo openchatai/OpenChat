03669038a055e3338f4204349656b44a5b7074f5
lvalics <lvalics@gmail.com>
2023-10-26 20:51:17 +0000

    CORS issue fiexed 2.

--- dj_backend_server/api/middleware/cors_middleware.py
+++ dj_backend_server/api/middleware/cors_middleware.py
@@ -8,8 +8,8 @@ class CorsMiddleware(MiddlewareMixin):
 
         # Check if the origin is in the database
         origin_in_db = Chatbot.objects.filter(website=origin).exists()
-        print(f"Origin of the request: {origin}")
-        print(f"Is the origin in the database: {origin_in_db}")
+        # print(f"Origin of the request: {origin}")
+        # print(f"Is the origin in the database: {origin_in_db}")
 
         if origin_in_db:
             # Add the 'Access-Control-Allow-Origin' header to the response
@@ -17,10 +17,10 @@ class CorsMiddleware(MiddlewareMixin):
             response['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
             response['Access-Control-Allow-Headers'] = 'X-Requested-With, Content-Type, X-Bot-Token'
 
-        print(f"Website URLs checked: {[chatbot.website for chatbot in Chatbot.objects.all()]}")
-        print(f"Response status code: {response.status_code}")
-        print(f"Response content: {response.content}")
-        print(f"Response headers: {response.headers}")
+        # print(f"Website URLs checked: {[chatbot.website for chatbot in Chatbot.objects.all()]}")
+        # print(f"Response status code: {response.status_code}")
+        # print(f"Response content: {response.content}")
+        # print(f"Response headers: {response.headers}")
 
 
         return response
