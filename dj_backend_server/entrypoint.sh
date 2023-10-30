c8a7bc76c2679c4f9fee7508148f459a2ec19fdf
lvalics <lvalics@gmail.com>
2023-10-30 05:35:56 +0000

    Added chat language variables into JS file for localization.

--- dj_backend_server/entrypoint.sh
+++ dj_backend_server/entrypoint.sh
@@ -8,6 +8,10 @@ SEARCH_JS_FILE="/app/web/static/search.js"
 sed -i "s|http://0.0.0.0:8000|${APP_URL}|g" $CHAT_JS_FILE
 sed -i "s|http://0.0.0.0:8000|${APP_URL}|g" $SEARCH_JS_FILE
 
+#Bot is Thinking
+sed -i "s|Bot is Thinking...|${LNG_BOT}|g" $CHAT_JS_FILE
+sed -i "s|error sending the message.|${LNG_ERROR}|g" $CHAT_JS_FILE
+
 # Check if the pattern with APP_URL already exists
 if grep -q '("${APP_URL}/api/chat/init")' $CHAT_JS_FILE; then
   echo "Pattern with APP_URL already exists, doing nothing."
@@ -21,7 +25,6 @@ elif grep -q '("http://0.0.0.0:8000/api/chat/init")' "$CHAT_JS_FILE"; then
 elif grep -q 'n.get("/api/chat/init")' "$CHAT_JS_FILE"; then
   echo "Appending APP_URL to /chat/init."
   sed -i "s|n.get(\"/api/chat/init\")|n.get(\"${APP_URL}/api/chat/init\")|g" "$CHAT_JS_FILE"
-fi
 
 # Check if the pattern n.get("api/chat/init") exists
 elif grep -q 'n.get("/chat/init")' "$CHAT_JS_FILE"; then
