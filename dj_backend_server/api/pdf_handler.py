0e09fe1ebf03eec8f6ce66b2b3a2310f8a4fc581
lvalics <lvalics@gmail.com>
2024-01-10 15:15:49 +0000

    Started to add API datasource.

--- dj_backend_server/api/pdf_handler.py
+++ dj_backend_server/api/pdf_handler.py
@@ -7,13 +7,9 @@ processing of the PDFs. The endpoint requires a POST request with the PDF files 
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
-from django.shortcuts import get_object_or_404
 from web.services.handle_pdf_datasource import HandlePdfDataSource
 from web.models.chatbot import Chatbot
 from web.signals.pdf_datasource_was_added import pdf_data_source_added
-from web.enums.chatbot_initial_prompt_enum import ChatBotInitialPromptEnum
-from web.enums.common_enums import ChatBotDefaults
-from uuid import uuid4
 
 @csrf_exempt
 @require_POST
@@ -31,22 +27,16 @@ def upload_pdf_api(request):
     except Chatbot.DoesNotExist:
         return JsonResponse({'error': 'Invalid token'}, status=403)
 
-    if request.method == 'POST':
-        """
-        Handles the POST request to upload PDF files. It extracts the bot token, processes the uploaded files,
-        creates a data source, and triggers an event to indicate that the PDF data source was added.
-        """
+    delete_folder_flag = request.POST.get('delete_folder_flag', '0') == '1'
+    files = request.FILES.getlist('pdffiles')
+    text_data = request.POST.get('text_data', '')
 
-        delete_folder_flag = request.POST.get('delete_folder_flag', '0') == '1'
+    # Handle the PDF data source
+    handle_pdf = HandlePdfDataSource(bot, files, text_data)
+    data_source = handle_pdf.handle()
+    print (f"text_data: {data_source}")
 
-        files = request.FILES.getlist('pdffiles')
-        # Handle the PDF data source
-        handle_pdf = HandlePdfDataSource(bot, files)
-        data_source = handle_pdf.handle()
-        # print (data_source)
-
-        # Trigger the PdfDataSourceWasAdded event
-        pdf_data_source_added.send(sender='create_via_pdf_flow', bot_id=bot.id, data_source_id=data_source.id, delete_folder_flag=delete_folder_flag)
-        return JsonResponse({'message': 'PDF uploaded and chatbot created successfully', 'data_source_id': data_source.id, 'bot_id': bot.id})
-
-    return JsonResponse({'error': 'Invalid request method'}, status=405)
+    # Trigger the PdfDataSourceWasAdded event
+    pdf_data_source_added.send(sender='create_via_pdf_flow', bot_id=bot.id, data_source_id=data_source.id, delete_folder_flag=delete_folder_flag)
+    return JsonResponse({'message': 'PDF uploaded and chatbot created successfully', 'data_source_id': data_source.id, 'bot_id': bot.id})
+    
--- dj_backend_server/web/services/handle_pdf_datasource.py
+++ dj_backend_server/web/services/handle_pdf_datasource.py
@@ -11,9 +11,10 @@ from uuid import uuid4
 import secrets
 
 class HandlePdfDataSource:
-    def __init__(self, bot: Chatbot, files):
+    def __init__(self, bot: Chatbot, files, text_data=None):
         self.bot = bot
         self.files = files
+        self.text_data = text_data
 
     def handle(self) -> PdfDataSource:
         data_source = PdfDataSource()
@@ -22,57 +23,74 @@ class HandlePdfDataSource:
         folder_name = secrets.token_hex(20)
         folder_path = f"website_data_sources/{folder_name}"
 
-        files_urls = []
-        files_info_list = []
-        for file in self.files:
-            try:
-                # Validate file types or other conditions if necessary
-                # For example: if not file.name.endswith('.pdf'): raise ValidationError('Invalid file type')
+        # If files are provided, process them
+        if self.files:
+            files_urls = []
+            files_info_list = []
+            for file in self.files:
+                try:
+                    # Validate file types or other conditions if necessary
+                    # For example: if not file.name.endswith('.pdf'): raise ValidationError('Invalid file type')
 
-                # Generate a unique file name using UUID
-                file_extension = os.path.splitext(file.name)[1]
-                file_uuid_name = str(uuid4()) + file_extension
-                file_path = os.path.join(folder_path, file_uuid_name)
+                    # Generate a unique file name using UUID
+                    file_extension = os.path.splitext(file.name)[1]
+                    file_uuid_name = str(uuid4()) + file_extension
+                    file_path = os.path.join(folder_path, file_uuid_name)
 
-                # Generate hash of the file content
-                file_hash = hashlib.md5(file.read()).hexdigest()
-                file.seek(0)  # Reset file pointer to beginning
-                
-                # Create the directory if it does not exist
-                directory = os.path.dirname(file_path)
-                os.makedirs(directory, exist_ok=True)
+                    # Generate hash of the file content
+                    file_hash = hashlib.md5(file.read()).hexdigest()
+                    file.seek(0)  # Reset file pointer to beginning
+                    
+                    # Create the directory if it does not exist
+                    directory = os.path.dirname(file_path)
+                    os.makedirs(directory, exist_ok=True)
 
-                # Check if the directory was created successfully
-                if not os.path.isdir(directory):
-                    raise Exception(f"Failed to create directory: {directory}")
+                    # Check if the directory was created successfully
+                    if not os.path.isdir(directory):
+                        raise Exception(f"Failed to create directory: {directory}")
 
-                # Save the file to the storage system
-                default_storage.save(file_path, file)
+                    # Save the file to the storage system
+                    default_storage.save(file_path, file)
 
-                # Save file info
-                files_info = {
-                    'original_name': file.name,
-                    'uuid_name': file_uuid_name,
-                    'hash': file_hash
-                }
-                files_urls.append(file_path)
-                files_info_list.append(files_info)
+                    # Save file info
+                    files_info = {
+                        'original_name': file.name,
+                        'uuid_name': file_uuid_name,
+                        'hash': file_hash
+                    }
+                    files_urls.append(file_path)
+                    files_info_list.append(files_info)
 
-            except Exception as e:
-                # Log the exception for debugging purposes
-                # print(f"Error while uploading file: {file.name}, Error: {str(e)}")
-                # You can log the exception to a file or use a proper logging framework
-                failed_job = FailedJob(uuid=str(uuid4()), connection='default', queue='default', payload=str(files_info_list), exception=str(e),
- failed_at=datetime.now())
-                failed_job.save()
-                # You can also raise a more specific custom exception if needed
-                raise ValidationError(f"Error while uploading file: {file.name}, Error: {str(e)}")
+                except Exception as e:
+                    # Log the exception for debugging purposes
+                    # print(f"Error while uploading file: {file.name}, Error: {str(e)}")
+                    # You can log the exception to a file or use a proper logging framework
+                    failed_job = FailedJob(uuid=str(uuid4()), connection='default', queue='default', payload=str(files_info_list), exception=str(e),
+    failed_at=datetime.now())
+                    failed_job.save()
+                    # You can also raise a more specific custom exception if needed
+                    raise ValidationError(f"Error while uploading file: {file.name}, Error: {str(e)}")
+
+            data_source.files = files_urls
+            data_source.files_info = files_info_list
+            data_source.folder_name = folder_name
+            data_source.ingest_status = 'File(s) Uploaded'
         
-        data_source.chatbot_id = self.bot.id
-        data_source.files = files_urls
-        data_source.files_info = files_info_list
-        data_source.folder_name = folder_name
-        data_source.ingest_status = 'PDF(s) Uploaded'
+        else:
+            files_urls = []
+            files_info_list = []
+            # If text_data is provided, use it
+            if self.text_data:
+                data_source.text_data = self.text_data
+                data_source.ingest_status = 'Text data processed'
+            else:
+                data_source.ingest_status = 'No files or text data provided'
 
+            data_source.files = files_urls
+            data_source.folder_name = "nofolderneeded"
+            data_source.files_info = self.text_data
+
+        data_source.chatbot_id = self.bot.id
         data_source.save()
+        print (f"datasource: {data_source}")
         return data_source
