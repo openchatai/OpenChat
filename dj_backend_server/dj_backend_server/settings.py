51a4a66d11e7f2eeb50981be8a5dd48e67096390
lvalics <lvalics@gmail.com>
2024-02-02 16:38:45 +0000

    Translation tags

--- dj_backend_server/dj_backend_server/settings.py
+++ dj_backend_server/dj_backend_server/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3v$%9tbrdnch+r&cv5p_30hcv363a*#zw_^-1s#76yh!$ej+x3'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
